e61432f178622e46b3e4be03dfa7b241f019a72a
Ivan Quintana <ivanquisco@gmail.com>
2022-04-09 19:36:11 -0500

    Adding new library for testing helpers

---
+++ Sources/CoolSwiftTesting/DateUtilities.swift
@@ -0,0 +1,16 @@
+//
+//  DateUtilities.swift
+//  
+//
+//  Created by Ivan Quintana on 09/04/22.
+//
+
+import Foundation
+
+#if DEBUG
+
+public extension Date {
+    static let test1970: Self = .init(timeIntervalSince1970: 0)
+}
+
+#endif
